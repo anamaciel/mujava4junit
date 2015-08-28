0861129311fac431a0952c9b684f1e795d7b1850
anamaciel <anacm.maciel@gmail.com>
2015-08-28 15:46:45 -0300

    DCfTV working

--- mujava_newV2/src/mujava/SignatureMutantsGenerator.java
+++ mujava_newV2/src/mujava/SignatureMutantsGenerator.java
@@ -23,6 +23,7 @@ import java.io.PrintWriter;
 import mujava.op.basic.CreateDirForEachMethod;
 import mujava.op.oracle.ASM;
 import mujava.op.oracle.ATV;
+import mujava.op.oracle.DCfTV;
 import mujava.op.oracle.MSM;
 import mujava.op.oracle.RBA;
 import mujava.op.oracle.RFM;
@@ -241,14 +242,14 @@ public class SignatureMutantsGenerator  extends MutantsGenerator
 						Debug.println("  Applying ICtTV ... ... ");
 						mutant_op = new ROR(file_env, cdecl, comp_unit);
 						comp_unit.accept(mutant_op);
-					}
+					}*/
 
 					if (hasOperator (signatureOp, "DCfTV") )
 					{
 						Debug.println("  Applying DCfTV ... ... ");
-						mutant_op = new COR(file_env, cdecl, comp_unit);
+						mutant_op = new DCfTV(file_env, cdecl, comp_unit);
 						comp_unit.accept(mutant_op);
-					}*/
+					}
 
 					if (hasOperator (signatureOp, "MSM") ) 
 					{
---
+++ mujava_newV2/src/mujava/op/oracle/DCfTV.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (C) 2015  the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package mujava.op.oracle;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import openjava.mop.FileEnvironment;
+import openjava.mop.OJClass;
+import openjava.ptree.ClassDeclaration;
+import openjava.ptree.CompilationUnit;
+import openjava.ptree.ExpressionList;
+import openjava.ptree.Literal;
+import openjava.ptree.MethodCall;
+import openjava.ptree.ParseTreeException;
+/**
+ * <p>Generate DCfTV (Decrement Constant from Threshold Value) mutants --
+ *    Example: assertEquals(double, double, 0.0001) → assertEquals(double, double, 0.0001-0.000001)
+ *    
+ *    assertEquals
+ *    assertArrayEquals
+ *    assertNotEquals
+ *    
+ * </p>
+ * @author Ana Maciel
+ * @version 1.0
+ */
+
+public class DCfTV extends JUnit_OP
+{
+
+	public DCfTV(FileEnvironment file_env, ClassDeclaration cdecl, CompilationUnit comp_unit)
+	{
+		super( file_env, comp_unit );
+	}
+
+
+	public void visit( MethodCall p ) throws ParseTreeException
+	{
+
+		ExpressionList arguments = p.getArguments();
+		System.out.println(p.getName());
+
+		if ((p.getName().equals("assertEquals"))||(p.getName().equals("assertNotEquals"))
+				||(p.getName().equals("assertArrayEquals")))
+		{
+			try {
+				//System.out.println("environment: " + getEnvironment());
+				OJClass varType = arguments.get(1).getType(getEnvironment());
+				System.out.println(varType.getName());		   
+				ExpressionList mutantArgs = new ExpressionList();
+				System.out.println("qtde argumentos: " + arguments.size());
+				
+				if(arguments.size()==3 && (!arguments.get(0).getType(getEnvironment()).getName().contains("String"))){
+
+					mutantArgs.add(arguments.get(0));
+					mutantArgs.add(arguments.get(1));
+					mutantArgs.add(Literal.makeLiteralIncrementDouble(arguments.get(2)+""));
+					MethodCall mutant = new MethodCall(p.getReferenceExpr(), p.getName(), mutantArgs);
+					System.out.println(p);
+					System.out.println(mutant);
+					outputToFile(p, mutant);
+					
+				}else if(arguments.size()==4){
+					
+					mutantArgs.add(arguments.get(0));
+					mutantArgs.add(arguments.get(1));
+					mutantArgs.add(arguments.get(2));
+					mutantArgs.add(Literal.makeLiteralIncrementDouble(arguments.get(3)+""));
+					MethodCall mutant = new MethodCall(p.getReferenceExpr(), p.getName(), mutantArgs);
+					System.out.println(p);
+					System.out.println(mutant);
+					outputToFile(p, mutant);
+					
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	/**
+	 * Write RBA mutants to files
+	 * @param original_field
+	 * @param mutant
+	 */
+	public void outputToFile(MethodCall original_field, MethodCall mutant)
+	{
+		if (comp_unit == null) 
+			return;
+
+		String f_name;
+		num++;
+		f_name = getSourceName("DCfTV");
+		String mutant_dir = getMuantID("DCfTV");
+
+		try 
+		{
+			PrintWriter out = getPrintWriter(f_name);
+			System.out.println("f_name: " + f_name);
+			DCfTV_Writer writer = new DCfTV_Writer(mutant_dir, out);
+			writer.setMutant(original_field, mutant);
+			System.out.println(mutant);
+			//System.out.println(currentMethodSignature);
+			writer.setMethodSignature(currentMethodSignature);
+			comp_unit.accept( writer );
+			out.flush();  
+			out.close();
+		} catch ( IOException e ) {
+			System.err.println( "fails to create " + f_name );
+		} catch ( ParseTreeException e ) {
+			System.err.println( "errors during printing " + f_name );
+			e.printStackTrace();
+		}
+	}
+}
